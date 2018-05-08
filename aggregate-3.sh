8c298a3a6e4cbecfe8d324362236bd5431e296a3
Curt Dodds <dodds@hawaii.edu>
2018-05-07 20:25:05 -1000

    Added comments to the shell scripts

--- aggregate-3.sh
+++ aggregate-3.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
-BASE=${BASE:-by}
-ENTITY='SMA Keck NRAO Subaru ATLAS MOPS PS_PS UH88 EAO_JAC IRTF CFHT'
-#ENTITY='EAO_JAC'
+# Two input arguments are required, from date and to date
+# USe format YYYY-MM-DD
 SUFFIX="$1-$2"
 export FROM="$1"
 export TO="$2"
+
+# BASE determines the second component of the output file name
+BASE=${BASE:-by}
+# An ENTITY maps to the NfSen Profile Channels that were configured
+ENTITY='SMA Keck NRAO Subaru ATLAS MOPS PS_PS UH88 EAO_JAC IRTF CFHT'
+
+# Two output files are produced
+# One by classC
+# One by AS
 OUTFILE=from-${BASE}-classC-$SUFFIX.csv
 OUTFILE2=from-${BASE}-AS-$SUFFIX.csv
 
@@ -15,6 +23,7 @@ echo $SUFFIX
 for entity in $ENTITY
 do
   echo "Processing $entity flows for ${SUFFIX} ..."
+  # Below is a somewhat complex translation of data size abbreviations into numeric bytes
   cat flows.${entity}-${SUFFIX} | sed 's/\.[0-9]*|/\.0/' | awk 'BEGIN{IGNORECASE = 1}
        function printpower(n,b,p) {printf "%u ", n*b^p; }
        /[0-9]$/{print $2, $1};
@@ -26,10 +35,13 @@ do
        /MB$/{    print $1, printpower($2, 10,  6)};
        /GB$/{    print $1, printpower($2, 10,  9)};
        /TB$/{    print $1, printpower($2, 10, 12)}'| awk '{split($2,ipseg,"."); printf("%03d%03d%03d%03d|%d\n", ipseg[1], ipseg[2], ipseg[3], ipseg[4], $1);}' | awk '-F|' '{sum[$1] += $2;} END {for(subnet in sum) printf("%s|%s\n", subnet, sum[subnet]);}' | sort '--field-separator=|' >flow
+  # Perform a left outer join between the flows and the classC to AS map that was produced by the map-2.sh script
   join -t '|' -a 1 flow classC-lkup.csv | sed "s/^/${FROM}|${TO}|${entity}|/g" >>$OUTFILE.tmp
 done
 
+# Output the by classC output file
 awk '-F|' '{ip=$4; printf("%s|%s|%s|%d.%d.%d.%d|%s|%s|%s|%s|%s|%s\n", $1, $2, $3, ip/1e9, ip%1e9/1e6, ip%1e6/1e3, ip%1e3, $5, $6, $7, $8, $9, $10);}' $OUTFILE.tmp | sort '--field-separator=|' -Vr --key=3,3d --key=5,5 >$OUTFILE
 
+# Output the by AS output file
 awk '-F|' '{key=$3"-"$6; sum[key] += $5; hdr[key]=$1"|"$2"|"$3; tlr[key]=$6"|"$7"|"$8"|"$9"|"$10;} END {for(asn in sum) printf("%s|%s|%s\n", hdr[asn], sum[asn], tlr[asn]);}' $OUTFILE | sort '--field-separator=|' -Vr --key=3,3d --key=4,4 >$OUTFILE2 
 rm -f flow $OUTFILE.tmp
--- map-2.sh
+++ map-2.sh
@@ -1,26 +1,35 @@
 #!/bin/bash
 
+# Two input arguments are required, from date and to date
+# USe format YYYY-MM-DD
 FROM=$1
 TO=$2
 export FROM=${FROM:=2018-1-1}
 export TO=${TO:=2018-1-1}
 
+# An ENTITY maps to the NfSen Profile Channels that were configured
+ENTITY='SMA Keck NRAO Subaru ATLAS MOPS PS_PS UH88 EAO_JAC IRTF CFHT'
+
+# Create a file that maps classC subnets to AS information
 OUTFILE=classC-lkup.csv
 rm ip_info $OUTFILE 
 
-ENTITY='SMA Keck NRAO Subaru ATLAS MOPS PS_PS UH88 EAO_JAC IRTF CFHT'
+# Ignore small flows
+# This should be an input parameter or come from the env
 MINFLOW="500K"
 
 startdate=$(date -I -d "$FROM") || exit -1
 enddate=$(date -I -d "$TO")     || exit -1
 enddate=$(date -I -d "$enddate + 1 day")
 
+# For each day in the date range (inclusive)
 d="$startdate"
 while [ "$d" != "$enddate" ]
 do
   yy=$(date +%Y -d $d)
   mm=$(date +%m -d $d)
   dd=$(date +%d -d $d)
+  # For each NfSen Channel's subnets
   for entity in $ENTITY
   do
     echo "Processing $entity flows for ${yy}${mm}${dd} ..."
@@ -28,16 +37,19 @@ do
     M_val="/home/nfsen/profiles-data/Astro_Summary/${entity}"
     R_val="$TIMEWINDOW"
 
+    # get the flows
     nfdump -M ${M_val} -R ${R_val} -T -q -a -A dstip4/24 -L $MINFLOW "not dst net  128.171.0.0/16" |\
     grep -v "Byte limit:" | awk '{print $5 }' | while read p; 
     do
       echo "IP: $p" >> ip_info;
+      # get AS info for each subnet
       geoiplookup $p >> ip_info ;
     done
   done
   d=$(date -I -d "$d + 1 day")
 done
 
+# reformat the geoiplookup results for subsequent use
 python processAS.py ip_info $OUTFILE.tmp
 sort '--field-separator=|' $OUTFILE.tmp >$OUTFILE
 rm -f $OUTFILE.tmp
